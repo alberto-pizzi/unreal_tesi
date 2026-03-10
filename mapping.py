90c5568805ddd693bae1275e9973e33a3e4ce643
alberto-pizzi <alberto.pizzi2002@outlook.com>
2026-03-10 16:36:13 +0100

    Aggiunta mappatura morph e creazione delle curve e keyframes

--- mapping.py
+++ mapping.py
@@ -2,6 +2,86 @@ import unreal
 import csv
 import os
 
+#mapping
+arkit_to_metahuman = {
+
+# Eyes
+"EyeBlinkLeft": "eye_blink_L",
+"EyeBlinkRight": "eye_blink_R",
+"EyeLookDownLeft": "eye_lookDown_L",
+"EyeLookDownRight": "eye_lookDown_R",
+"EyeLookInLeft": "eye_lookIn_L",
+"EyeLookInRight": "eye_lookIn_R",
+"EyeLookOutLeft": "eye_lookOut_L",
+"EyeLookOutRight": "eye_lookOut_R",
+"EyeLookUpLeft": "eye_lookUp_L",
+"EyeLookUpRight": "eye_lookUp_R",
+"EyeSquintLeft": "eye_squint_L",
+"EyeSquintRight": "eye_squint_R",
+"EyeWideLeft": "eye_widen_L",
+"EyeWideRight": "eye_widen_R",
+
+# Brows
+"BrowDownLeft": "brow_down_L",
+"BrowDownRight": "brow_down_R",
+"BrowInnerUp": "brow_raiseInner",
+"BrowOuterUpLeft": "brow_raiseOuter_L",
+"BrowOuterUpRight": "brow_raiseOuter_R",
+
+# Cheeks
+"CheekPuff": "cheek_puff",
+"CheekSquintLeft": "cheek_squint_L",
+"CheekSquintRight": "cheek_squint_R",
+
+# Nose
+"NoseSneerLeft": "nose_sneer_L",
+"NoseSneerRight": "nose_sneer_R",
+
+# Jaw
+"JawOpen": "jaw_open",
+"JawForward": "jaw_forward",
+"JawLeft": "jaw_left",
+"JawRight": "jaw_right",
+
+# Mouth
+"MouthClose": "mouth_close",
+"MouthFunnel": "mouth_funnel",
+"MouthPucker": "mouth_pucker",
+"MouthLeft": "mouth_left",
+"MouthRight": "mouth_right",
+
+"MouthSmileLeft": "mouth_cornerPull_L",
+"MouthSmileRight": "mouth_cornerPull_R",
+
+"MouthFrownLeft": "mouth_cornerDepress_L",
+"MouthFrownRight": "mouth_cornerDepress_R",
+
+"MouthDimpleLeft": "mouth_dimple_L",
+"MouthDimpleRight": "mouth_dimple_R",
+
+"MouthStretchLeft": "mouth_stretch_L",
+"MouthStretchRight": "mouth_stretch_R",
+
+"MouthRollLower": "mouth_rollLower",
+"MouthRollUpper": "mouth_rollUpper",
+
+"MouthShrugLower": "mouth_shrugLower",
+"MouthShrugUpper": "mouth_shrugUpper",
+
+"MouthPressLeft": "mouth_press_L",
+"MouthPressRight": "mouth_press_R",
+
+"MouthLowerDownLeft": "mouth_lowerDown_L",
+"MouthLowerDownRight": "mouth_lowerDown_R",
+
+"MouthUpperUpLeft": "mouth_upperUp_L",
+"MouthUpperUpRight": "mouth_upperUp_R",
+
+# Tongue
+"TongueOut": "tongue_out"
+}
+
+
 # =========================
 # CONFIG
 # =========================
@@ -32,17 +112,84 @@ anim_seq = asset_tools.create_asset(ASSET_NAME, ASSET_PATH, unreal.AnimSequence,
 # =========================
 # LEGGI CSV
 # =========================
+prefix = "blendShapes."
+time_code_tag = "timeCode"
+
 with open(CSV_PATH, newline='') as f:
     reader = csv.DictReader(f)
-    reader.fieldnames = [n.strip() for n in reader.fieldnames]
-    rows = [row for row in reader if row and row.get("timeCode", "").strip() != ""]
+    #tolgo il prefisso a tutti i nomi dei blendshape
+    cleaned_keys  = [key.strip().removeprefix(prefix)
+                         for key in reader.fieldnames if key.strip() != '']
+    arkit_csv_names = cleaned_keys.copy()
+    rows = []
+    for row in reader:
+        new_row = {new_key: row[old_key] for old_key, new_key in zip(reader.fieldnames, cleaned_keys)}
+        rows.append(new_row)
+
+
+#rimuovo primo elemento "timeCode"
+arkit_csv_names.pop(0)
 
 if not rows:
     raise RuntimeError("CSV vuoto o malformato")
 
+print("CSV letto correttamente")
 # nomi delle curve
-curve_names = [c.replace("blendShapes.", "") for c in rows[0].keys() if c != "timeCode"]
-print(curve_names)
+#curve_names = [c.replace("blendShapes.", "") for c in rows[0].keys() if c != "timeCode"]
+#print(curve_names)
+
+'''
+# imposto durata animazione
+last_time = float(rows[-1][time_code_tag])
+anim_seq.set_editor_property("sequence_length", last_time)
+anim_seq.set_editor_property("number_of_frames", len(rows)-1)
+
+print("Durate impostate")
+
+'''
+
+mh_prefix = "head_lod0_mesh__"
+#creo list morph mappati (da arkit a metahuman)
+morphs_mh = []
+for arkit_name in arkit_csv_names:
+    if arkit_name in arkit_to_metahuman:
+        morph_mh = mh_prefix + arkit_to_metahuman[arkit_name]
+        morphs_mh.append(morph_mh)
+
+print("Mappatura completa")
+
+
+
+#creo curve per ogni blendshape (i morphs mappati sono 52)
+for name_mh in morphs_mh:
+    unreal.AnimationLibrary.add_curve(anim_seq, name_mh,unreal.RawCurveTrackTypes.RCT_FLOAT)
+
+print("Tutte le curve aggiunte")
+#inserire keyframe
+
+print(list(zip(morphs_mh, arkit_csv_names)))
+
+
+for name_mh, arkit_name in zip(morphs_mh, arkit_csv_names):
+    times = unreal.Array(float)
+    values = unreal.Array(float)
+
+    for row in rows:
+        times.append(float(row[time_code_tag]))
+        values.append(float(row[arkit_name]))
+
+    """
+    if c <1000:
+        print(arkit_name,": ")
+        print("time: ")
+        print(times)
+        print("value: ")
+        print(values)
+    """
+
+    unreal.AnimationLibrary.add_float_curve_keys(anim_seq, name_mh,times,values)
+
+
 
 """
 # =========================
