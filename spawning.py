00dbe79b2c19a5e26800a2ba632d01e2f84a6985
alberto-pizzi <alberto.pizzi2002@outlook.com>
2026-03-12 18:49:18 +0100

    Aggiunto selezione,spawn e despawn attore in base al nome

---
+++ spawning.py
@@ -0,0 +1,42 @@
+import unreal
+
+MH_BASE_PATH = "/Game/MetaHumans/"
+
+def select_actor(mh_official_name):
+    actor_subsystem = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
+    all_elements = actor_subsystem.get_all_level_actors()
+    real_actor = None
+    for actor in all_elements:
+        if mh_official_name in actor.get_name():
+            real_actor = actor
+            break
+    return real_actor
+
+def spawn_metahuman(mh_official_name):
+    actor_subsystem = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
+    bp_path = MH_BASE_PATH + mh_official_name + "/BP_" + mh_official_name + ".BP_" + mh_official_name
+    location = unreal.Vector(0,0,0)
+    rotation = unreal.Rotator(0, 0, 0)
+    bp_class = unreal.EditorAssetLibrary.load_blueprint_class(bp_path)
+    new_actor = actor_subsystem.spawn_actor_from_class(bp_class, location, rotation)
+    print("Spawnato!")
+    return new_actor
+
+def despawn_metahuman(actor_obj):
+    actor_subsystem = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
+    return actor_subsystem.destroy_actor(actor_obj)
+
+
+
+if __name__ == "__main__":
+    print("Inizio Main!")
+    actor = select_actor("Bernice")
+    if actor:
+        print("Attore selezionato")
+        despawn_metahuman(actor)
+        print("Despawn termianto")
+    else:
+        print("Non selezionato")
+
+    print("Fine Main!")
+
