cdbcdf76c63ae005f852f6054cd9d3e94d6d55b1
alberto-pizzi <alberto.pizzi2002@outlook.com>
2026-03-12 22:26:27 +0100

    Fixato calcolo parametri di posizione actor camera

--- spawning.py
+++ spawning.py
@@ -31,11 +31,21 @@ def despawn_metahuman(actor_obj):
 def spawn_camera_actor(actor_obj):
     actor_subsystem = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
     camera_class = unreal.CameraActor.static_class()
-    # TODO locazione da impostare
-    spawn_location = unreal.Vector(0.0, 0.0, 300.0)
-    spawn_rotation = unreal.Rotator(0.0, 0.0, 0.0)
+
+    skeletal = actor_obj.get_component_by_class(unreal.SkeletalMeshComponent)
+    head_transform = skeletal.get_socket_transform("head", unreal.RelativeTransformSpace.RTS_WORLD)
+    head_location = head_transform.translation
+    offset = unreal.Vector(0, 40, 4)
+    camera_location = head_location + offset
+    rotation = unreal.Rotator(0.0, 0.0, -90.0)
+
     # Spawn del CameraActor nella posizione e rotazione specificate
-    camera_actor = actor_subsystem.spawn_actor_from_class(camera_class, spawn_location, spawn_rotation)
+    camera_actor = actor_subsystem.spawn_actor_from_class(camera_class, camera_location, rotation)
+
+    #imposta parametri di fuoco
+    camera_component = camera_actor.get_component_by_class(unreal.CameraComponent.static_class())
+    camera_component.set_editor_property("field_of_view",75.0)
+    camera_component.set_editor_property("aspect_ratio", 1.5)
 
     if camera_actor:
         camera_actor.set_actor_label("ActorCam")
