4f549a1e700a737201bb66a046432a7121973ec8
alberto-pizzi <alberto.pizzi2002@outlook.com>
2026-03-14 23:33:06 +0100

    Refactoring

--- script_batch_csv.py
+++ script_batch_csv.py
@@ -1,78 +1,81 @@
 import os
 import subprocess
-import glob
-import grpc
+import sys
 from pathlib import Path
 
-import sys
 
-BASE_DIR = Path.cwd()
+BASE_DIR = Path(__file__).resolve().parent
 
-# Configurazioni
+# Settings
 API_KEY = "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"  # NVIDIA API
 FUNCTION_ID = "8efc55f5-6f00-424e-afe9-26212cd2c630"  #  Mark model
-CONFIG_YAML = BASE_DIR / "config" / "config_mark.yml"  # File YAM (mark) con emozioni
-AUDIO_DIR = BASE_DIR / "input_audio_files/*.wav"  # Cartella input
-OUTPUT_DIR = BASE_DIR / "output_csv" #cartella output
-CLIENT_DIR = BASE_DIR/"Audio2Face-3D-Samples"/"scripts"/"audio2face_3d_api_client"
-
-CLIENT_NAME = "nim_a2f_3d_client"
-
-print("PYTHON:", sys.executable)
-print("GRPC:", "grpc" in sys.modules or __import__("grpc"))
+AUDIO_DIR = BASE_DIR / "input_audio_files"  # input folder
+OUTPUT_FOLDER = "output_csv" # output folder
+CLIENT_NAME = "nim_a2f_3d_client.py"
+CLIENT_DIR = BASE_DIR / "Audio2Face-3D-Samples"/"scripts"/"audio2face_3d_api_client"
+OUTPUT_DIR = BASE_DIR / OUTPUT_FOLDER
+CONFIG_YAML = CLIENT_DIR / "config" / "config_mark.yml"  # file YAM (mark) with emotions
 
 
 
 def batch_a2f_csv():
     OUTPUT_DIR.mkdir(exist_ok=True)
+    # find all WAV files
+    wav_file_paths = list(AUDIO_DIR.glob("*.wav"))
+    print("Audio file paths: ", str(wav_file_paths))
 
+    for wav_file_path in wav_file_paths:
+        run_a2f(wav_file_path)
+
+def run_a2f(audio_file_path: Path):
     client_path = CLIENT_DIR / CLIENT_NAME
+    print("Client path: ", str(client_path))
 
-    print("Client path: ", client_path)
-    # Trova tutti i file WAV
-    audio_file_paths = glob.glob(str(AUDIO_DIR))
+    audio_filename = audio_file_path.stem
+    #output_blendshapes = OUTPUT_DIR / f"{audio_filename}_blendshapes.csv"
+    #print(str(output_blendshapes))
+    # print(str(audio_file_path))
 
-    env = os.environ.copy()
-    env["PYTHONPATH"] = f"{CLIENT_DIR}:{BASE_DIR}"
+    print(str(audio_filename))
 
-    for audio_file_path in audio_file_paths:
+    cmd = [
+        sys.executable, str(CLIENT_DIR/CLIENT_NAME),
+        str(audio_file_path), str(CONFIG_YAML),
+        "--apikey", API_KEY,
+        "--function-id", FUNCTION_ID
+    ]
 
-        audio_file_path = Path(audio_file_path)
+    print("Executing: ", cmd)
 
-        audio_filename = audio_file_path.stem
-        output_blendshapes = OUTPUT_DIR / f"{audio_filename}_blendshapes.csv"
+    print(f"Processing: {str(audio_file_path)}")
+    result = subprocess.run(cmd, capture_output=True, text=True, cwd=OUTPUT_DIR)
 
+    if result.returncode == 0:
+        folder = find_folder_names_by_prefix("2026")[0]
+        rename_folder(folder, audio_filename)
+        print(f"Completed: {audio_filename}")
+    else:
+        print(f"Error: {result.stderr}")
 
-        print(audio_file_path)
-        print(audio_filename)
-        print(output_blendshapes)
+#TODO conviene rinominare anche i csv interni?
+def rename_folder(old_folder_name, new_folder_name):
+    path = Path().cwd() #take current absolute path
+    folder_path = path /Path(OUTPUT_FOLDER)
+    os.rename(folder_path / old_folder_name, folder_path / new_folder_name)
+    print(f"Renamed: {old_folder_name} -> {new_folder_name}")
 
-        cmd = [
-            sys.executable,"-m", CLIENT_NAME,
-            audio_file_path, CONFIG_YAML,
-            "--apikey", API_KEY,
-            "--function-id", FUNCTION_ID
-        ]
+# WARNING: the prefix should be "2026" according to the outputs, because it corresponds to the year of creation of the output file
+def find_folder_names_by_prefix(prefix):
+    path = Path().cwd() #take current absolute path
+    folder_path = path /Path(OUTPUT_FOLDER)
+    folders = [d.stem for d in folder_path.iterdir() if d.is_dir() and d.name.startswith(prefix)]
+    return folders
 
-        print(f"Processing: {audio_file_path}")
-        result = subprocess.run(cmd, capture_output=True, text=True, cwd=CLIENT_DIR, env=env)
-
-        if result.returncode == 0:
-            print(f"Completed: {output_blendshapes}")
-        else:
-            print(f"Error: {result.stderr}")
-
-# FIXME da fixare tutto
 
 if __name__ == "__main__":
-    print("inizio main python")
+    print("start main python")
 
-
-    #print("prova_base: ", Path.cwd())
     batch_a2f_csv()
-    print("fine main python")
 
-
-    #subprocess.run("echo Ciao dal terminale", shell=True).
-    #subprocess.run("python script_batch_csv2.py", shell=True)
+    print("end main python")
 
